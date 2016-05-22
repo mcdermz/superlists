0cd953b734e528caee4687330aac7f099b1a006a
mcdermz <mcderm.sean@gmail.com>
2016-05-22 15:14:17 -0700

    New URL and View for adding to existing lists. FT passes

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,9 +17,15 @@ from django.conf.urls import url
 #from django.contrib import admin
 from lists import views
 
+
+''' 
+	In Django 1.10 the 2nd argument of the URL does not need quotes. 
+    In Django 1.8 the 2nd argument must be passed as a string
+'''
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     #url(r'^admin/', admin.site.urls),
 ]
