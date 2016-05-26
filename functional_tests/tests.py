b703ee4da794bf4ddabe17319d9ea271c8e7c0bb
mcdermz <mcderm.sean@gmail.com>
2016-05-26 14:17:25 -0700

    Changed 3 lines in FT for staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	
 		# Edith has heard about a new online to-do app.
 		# She goes to check out its homepage:
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 		
 		# She notices the input box is nicely centered:
@@ -62,7 +62,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		
 		# She notices the page title and header mentions to-do lists:
 		self.assertIn('To-Do', self.browser.title)
@@ -112,7 +112,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 		
 		# Francis visits the home page, there is no sign of Edith's list.
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
